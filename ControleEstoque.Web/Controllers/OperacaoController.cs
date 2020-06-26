0ca858acba22ae5fcd5929b4961c8ca7fc51ad7a
unknown <danieltolezani@outlook.com>
2020-06-26 10:19:41 -0300

    Entrada de Produtos em Estoque

--- ControleEstoque.Web/Controllers/OperacaoController.cs
+++ ControleEstoque.Web/Controllers/OperacaoController.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
 
 namespace ControleEstoque.Web.Controllers
 {
     public class OperacaoController : Controller
     {
-        [Authorize]
-        public ActionResult EntradaEstoque()
-        {
-            return View();
-        }
 
         [Authorize]
         public ActionResult SaidaEstoque()
