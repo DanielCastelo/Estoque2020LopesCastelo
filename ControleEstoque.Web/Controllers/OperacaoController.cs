683ac614ac6d6eac0d2cf2c8928f40ecfdf9ff95
unknown <danieltolezani@outlook.com>
2020-07-02 09:42:49 -0300

    Inventário de Estoque - Back-End

--- ControleEstoque.Web/Controllers/OperacaoController.cs
+++ ControleEstoque.Web/Controllers/OperacaoController.cs
@@ -5,21 +5,8 @@ namespace ControleEstoque.Web.Controllers
 {
     public class OperacaoController : Controller
     {
-
-        [Authorize]
-        public ActionResult SaidaEstoque()
-        {
-            return View();
-        }
-
         [Authorize]
         public ActionResult LancPerdaProduto()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult Inventario()
         {
             return View();
         }
